5ba16846aeb697202553452f04f507aac37b4f57
Jeet Soni <jeet.soni0822@gmail.com>
2025-08-16 14:54:47 -0400

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Work_Sans, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Portfolio",
+  description: "Jeet Soni's Portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+      <html lang="en">
+          <body
+              className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+              {children}
+          </body>
+      </html>
+  );
+}
