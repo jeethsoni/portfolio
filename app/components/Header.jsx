6cfa23ca15f55a943c58968e05a8a1e6fdb694d0
Jeet Soni <jeet.soni0822@gmail.com>
2025-09-01 12:23:21 -0400

    add: Github Globe

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,11 +2,16 @@
 
 import dynamic from "next/dynamic";
 import Navbar from "./Navbar"
+import arcs from "@/data/arcs.json";
+
 
 
 const Galaxy = dynamic(() => import("./reactbits/backgrounds/Galaxy/Galaxy"), {
   ssr: false,              // prevents WebGL from running on the server
 });
+
+const GlobeWorld = dynamic(() => import("./ui/Globe").then(m => m.World), { ssr: false });
+
 
 export default function Header() {
   return (
@@ -15,13 +20,28 @@ export default function Header() {
         <Galaxy transparent={false} density={2.0} glowIntensity={0.5}/>
       </div>
 
+      <div className="absolute right-0 top-50 z-40 w-[300px] h-[300px] md:w-[400px] md:h-[400px] pointer-events-auto"
+      >
+        <GlobeWorld
+          globeConfig={{
+            globeColor: "#1e3a8a",
+            atmosphereColor: "#60A5FA",
+            showAtmosphere: true,
+          }}
+          data={arcs}
+        />
+      </div>
+
       <div className="absolute top-0 inset-x-0 z-20">
         <Navbar />
       </div>
 
-      <div className="relative z-10 flex items-center justify-center h-full text-white">
-        <h1 className="text-4xl md:text-6xl font-bold">Hi, I’m Jeet</h1>
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 text-center text-white z-20">
+        <h1>
+          Hi, I'm Jeet Soni
+        </h1>
       </div>
+
     </header>
   );
 }
