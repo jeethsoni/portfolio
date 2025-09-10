5189af782fa3e31589a9366638dbe742f4756547
Jeet Soni <jeet.soni0822@gmail.com>
2025-09-10 12:36:13 -0400

    add: profile picture and introduction

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,10 @@ import dynamic from "next/dynamic";
 import Navbar from "./Navbar";
 import arcs from "@/data/arcs.json";
 import Image from "next/image";
+import { GoDownload } from "react-icons/go";
 import { FlipWords } from "./ui/flip-words";
+import { IoMdArrowForward } from "react-icons/io";
+
 
 const Galaxy = dynamic(() => import("./reactbits/backgrounds/Galaxy/Galaxy"), { ssr: false });
 const GlobeWorld = dynamic(() => import("./ui/Globe").then((m) => m.World), { ssr: false });
@@ -23,31 +26,49 @@ export default function Header() {
       </div>
 
       {/* Content */}
-      <div className="relative z-20 container mx-auto px-4 md:px-6 py-12 md:py-20 mt-16">
+      <div className="relative z-20 container mx-auto px-4 md:px-6 py-12 md:py-20 mt-10">
         <div className="grid gap-8 lg:gap-12 lg:grid-cols-[1fr_400px] xl:grid-cols-[1fr_500px] items-start">
           
           {/* LEFT: Text */}
-          <div className="flex flex-col space-y-4 text-center md:text-left items-center md:items-start">
-            <h1 className="text-white font-bold leading-tight text-3xl sm:text-4xl md:text-5xl items-center">
+          <div className="flex flex-col text-center md:text-left items-center md:items-start">
+            <h1 className="text-white font-bold leading-tight text-3xl sm:text-4xl md:text-5xl sm:items-center">
               Hi<span className="ml-2 inline-block origin-[70%_70%] animate-wave align-middle">👋</span>, I'm {""} 
-              <span className="text-emerald-600">
+              <span className="text-blue-400">
                 Jeet Soni
                 </span>
             </h1>
 
-            <h2 className="sm:text-lg max-w-xl">
-                <FlipWords className={"text-2xl font-serif"}
-                  words={["Full Stack Developer", "Backend Developer", "Creator"]}
+          
+            <h2 className="sm:text-lg max-w-xl leading-tight">
+                <FlipWords className={"text-3xl font-flip font-normal [&_span]:text-emerald-500"}
+                  words={["Full Stack Developer", "Backend Developer", "Innovator", "AI/ML Practitioner", "Tech Enthusiast"]}
+                  duration={2000}
                 />
             </h2>
 
-            <div className="pt-2 flex flex-wrap gap-3 justify-center md:justify-start">
+            <p className="mt-2 text-md text-white/80 max-w-[600px] leading-relaxed md:text-lg">
+              A passionate developer with expertise in Express.js, FastAPI, PostgreSQL, and MongoDB. Skilled at building scalable REST APIs, optimizing data systems, and leveraging cloud technologies like AWS to deliver impactful, real-world solutions.
+            </p>
+            
+
+            <div className="pt-4 flex justify-center md:justify-start gap-4">
               <a
                 href="#resume"
-                className="rounded-2xl px-5 py-2.5 bg-white/10 backdrop-blur-md hover:bg-white/20 transition shadow"
+                className="px-4 py-2 rounded-lg border-2 text-white border-gray-50   backdrop-blur-md text-bold hover:bg-white/20 transition shadow flex items-center gap-2"
               >
                 Resume
+                <GoDownload className="inline-block" />
               </a>
+
+              <a
+                href="#mywork"
+                className="px-4 py-2 border-2 text-white border-gray-50 rounded-lg backdrop-blur-md text-bold hover:bg-white/20 transition shadow flex items-center gap-2"
+              >
+                View my work
+                <IoMdArrowForward className="inline-block" />
+              </a>
+
+
             </div>
           </div>
 
@@ -55,8 +76,8 @@ export default function Header() {
           <div className="flex flex-col items-center gap-8">
             <Image
               src="/profile_pic.png"
-              width={300}
-              height={300}
+              width={350}
+              height={350}
               alt="profile picture"
               className="rounded-2xl border-4 border-white/30 shadow-[0_0_25px_rgba(96,165,250,0.6)] transition-transform duration-300 hover:scale-105"
             />
@@ -70,6 +91,7 @@ export default function Header() {
                 data={arcs}
               />
             </div>
+            
           </div>
 
         </div>
