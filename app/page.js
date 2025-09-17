012bda5336016a442b6685641388033a387f2ae3
Jeet Soni <jeet.soni0822@gmail.com>
2025-09-16 21:09:18 -0400

    add: about page component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client";
+import About from "./components/About";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <About />
     </>
   );
 }
